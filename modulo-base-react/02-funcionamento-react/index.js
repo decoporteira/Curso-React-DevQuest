5654da64767c02795613cd6cd541455ac9ceea5b
Andre Pereira <andreporteira@gmail.com>
2023-10-01 15:42:03 -0300

    react

---
+++ modulo-base-react/02-funcionamento-react/index.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root');
+const p = React.createElement('p', null, 'Hello World!');
+const g = React.createElement('div', null, 'que isso');
+
+ReactDOM.render(p, root);
+ReactDOM.render(g, root);
+
