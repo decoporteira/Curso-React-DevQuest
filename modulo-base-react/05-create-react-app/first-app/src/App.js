306a92073b0bf30c1310428bf7988ad02363f050
André Pereira <andreporteira@gmail.com>
2024-03-19 11:39:28 -0300

    Componentes de função e classe

--- modulo-base-react/05-create-react-app/first-app/src/App.js
+++ modulo-base-react/05-create-react-app/first-app/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/button/button';
+import Card from './components/card/card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>    
+      <Card />
+      <Button />
+      <Card />
+    </>
+
   );
 }
 
---
+++ modulo-base-react/05-create-react-app/first-app/src/components/card/card.js
@@ -0,0 +1,16 @@
+/* card.js */
+import React from 'react'
+import './card.css'
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className='card'>
+        <h3>Título</h3>
+        <p>Esse é um texto do card.</p>
+      </div>
+    )
+  }
+}
+
+export default Card;
